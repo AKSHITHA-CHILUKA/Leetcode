4df37bd040acf37c8e3c671758adb2e0e5eda428
~Chiluka Akshitha <120377576+AKSHITHA-CHILUKA@users.noreply.github.com>
2026-02-03 05:40:57 +0530

    Create 3637. Trionic Array I.py

---
+++ Daily_Questions/3637. Trionic Array I.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isTrionic(self, nums: list[int]) -> bool:
+
+        n, ptr = len(nums), 0
+                                    # 1st up with while
+        while ptr < n - 1 and nums[ptr] < nums[ptr + 1]:
+            ptr+= 1
+        if ptr == 0 or ptr == n - 2:
+            return False
+
+        p = ptr                     # The down with while
+        while ptr < n - 1 and nums[ptr] > nums[ptr + 1]:
+            ptr+= 1
+        if ptr == p or ptr == n - 1:
+            return False
+                                    # 2nd up with for
+        for left, rght in pairwise(nums[ptr:]):
+            if left >= rght:
+                return False
+
+        return True
